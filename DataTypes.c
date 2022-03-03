bfdc39850d997a054cdb27c647fe2798bea85c8d
Abhinandan R <abhiace123456@gmail.com>
2022-03-03 20:40:16 +0530

    New program

---
+++ DataTypes.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int age = 23;
+    char male = 'M';
+    char female = 'F';
+    char name [] = "Jonathan";
+
+    printf("Name of the person is %s\n", name);
+    printf("Age of the person is %d\n", age);
+    printf("Gender is %c\n", male);
+}
